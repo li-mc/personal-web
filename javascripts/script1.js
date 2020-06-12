ef235fe3633308351b8ae59327a06dfd5338a4ea
Li <mccarthy.com@husky.neu.edu>
2020-06-12 10:52:36 -0400

    add jquery

--- javascripts/script1.js
+++ javascripts/script1.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-	$(".catbtn").click(function(){
-   	$(".secretimg").show();
-	});
+
+  function randImg(){
+    let min=1;
+    let max=6;
+    let selection = Math.floor(Math.random()*(max-min+1))+min;
+    let img = "../resources/catpics/" + selection + ".png";
+    return img;
+  }
+
+  $(".catbtn").click(function(){
+     fileName = randImg();
+
+    $(".secretimg").attr("src", fileName);
+    $(".secretimg").css("top", (Math.random() * 200) + "px");
+    $(".secretimg").css("left", (Math.random() * 800) + "px");
+
+    $(".secretimg").show();
+
+  });
 
 });
